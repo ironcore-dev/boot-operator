f488db7d5e4ebf1a0bd51dd7f0e3fb2fc12539dd
Andreas Fritzler <andreas.fritzler@sap.com>
2025-12-09 14:47:11 +0100

    Harmonize reconciler logic and improve logging

--- Makefile
+++ Makefile
@@ -201,7 +201,7 @@ CONTROLLER_TOOLS_VERSION ?= v0.17.1
 ENVTEST_VERSION ?= $(shell go list -m -f "{{ .Version }}" sigs.k8s.io/controller-runtime | awk -F'[v.]' '{printf "release-%d.%d", $$2, $$3}')
 #ENVTEST_K8S_VERSION is the version of Kubernetes to use for setting up ENVTEST binaries (i.e. 1.31)
 ENVTEST_K8S_VERSION ?= $(shell go list -m -f "{{ .Version }}" k8s.io/api | awk -F'[v.]' '{printf "1.%d.%d",$$3, $$2}')
-GOLANGCI_LINT_VERSION ?= v2.1
+GOLANGCI_LINT_VERSION ?= v2.6
 ADDLICENSE_VERSION ?= v1.1.1
 GOIMPORTS_VERSION ?= v0.31.0
 CRD_REF_DOCS_VERSION ?= v0.2.0
--- internal/controller/ipxebootconfig_controller.go
+++ internal/controller/ipxebootconfig_controller.go
@@ -12,7 +12,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/go-logr/logr"
@@ -32,49 +31,47 @@ type IPXEBootConfigReconciler struct {
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 
 func (r *IPXEBootConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := log.FromContext(ctx)
-	IPXEBootConfig := &bootv1alpha1.IPXEBootConfig{}
-	if err := r.Get(ctx, req.NamespacedName, IPXEBootConfig); err != nil {
+	log := ctrl.LoggerFrom(ctx)
+	config := &bootv1alpha1.IPXEBootConfig{}
+	if err := r.Get(ctx, req.NamespacedName, config); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-
-	return r.reconcileExists(ctx, log, IPXEBootConfig)
+	return r.reconcileExists(ctx, log, config)
 }
 
-func (r *IPXEBootConfigReconciler) reconcileExists(ctx context.Context, log logr.Logger, IPXEBootConfig *bootv1alpha1.IPXEBootConfig) (ctrl.Result, error) {
-	if !IPXEBootConfig.DeletionTimestamp.IsZero() {
-		return r.delete(ctx, log, IPXEBootConfig)
+func (r *IPXEBootConfigReconciler) reconcileExists(ctx context.Context, log logr.Logger, config *bootv1alpha1.IPXEBootConfig) (ctrl.Result, error) {
+	if !config.DeletionTimestamp.IsZero() {
+		return r.delete(ctx, log, config)
 	}
-
-	return r.reconcile(ctx, log, IPXEBootConfig)
+	return r.reconcile(ctx, log, config)
 }
 
-func (r *IPXEBootConfigReconciler) reconcile(ctx context.Context, log logr.Logger, ipxeBootConfig *bootv1alpha1.IPXEBootConfig) (ctrl.Result, error) {
+func (r *IPXEBootConfigReconciler) reconcile(ctx context.Context, log logr.Logger, config *bootv1alpha1.IPXEBootConfig) (ctrl.Result, error) {
+	log.V(1).Info("Reconciling IPXEBootConfig")
+
 	log.V(1).Info("Ensuring Ignition")
-	state, ignitionErr := r.ensureIgnition(ctx, log, ipxeBootConfig)
-	if ignitionErr != nil {
-		patchError := r.patchStatus(ctx, ipxeBootConfig, state)
-		if patchError != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to patch status %w %w", ignitionErr, patchError)
+	state, err := r.ensureIgnition(ctx, log, config)
+	if err != nil {
+		if err := r.patchStatus(ctx, config, state); err != nil {
+			return ctrl.Result{}, err
 		}
+		return ctrl.Result{}, fmt.Errorf("failed to ensure Ignition: %w", err)
+	}
+	log.V(1).Info("Ensured Ignition")
 
-		log.V(1).Info("Failed to Ensure Ignition", "Error", ignitionErr)
-		return ctrl.Result{}, nil
+	if err := r.patchStatus(ctx, config, state); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to patch status %w", err)
 	}
 
-	patchErr := r.patchStatus(ctx, ipxeBootConfig, state)
-	if patchErr != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to patch status %w", patchErr)
-	}
-
+	log.V(1).Info("Reconciled IPXEBootConfig")
 	return ctrl.Result{}, nil
 }
 
-func (r *IPXEBootConfigReconciler) ensureIgnition(ctx context.Context, _ logr.Logger, ipxeBootConfig *bootv1alpha1.IPXEBootConfig) (bootv1alpha1.IPXEBootConfigState, error) {
+func (r *IPXEBootConfigReconciler) ensureIgnition(ctx context.Context, _ logr.Logger, config *bootv1alpha1.IPXEBootConfig) (bootv1alpha1.IPXEBootConfigState, error) {
 	// Verify if the IgnitionRef is set, and it has the intended data key.
-	if ipxeBootConfig.Spec.IgnitionSecretRef != nil {
+	if config.Spec.IgnitionSecretRef != nil {
 		IgnitionSecret := &corev1.Secret{}
-		if err := r.Get(ctx, client.ObjectKey{Name: ipxeBootConfig.Spec.IgnitionSecretRef.Name, Namespace: ipxeBootConfig.Namespace}, IgnitionSecret); err != nil {
+		if err := r.Get(ctx, client.ObjectKey{Name: config.Spec.IgnitionSecretRef.Name, Namespace: config.Namespace}, IgnitionSecret); err != nil {
 			return bootv1alpha1.IPXEBootConfigStateError, err
 			// TODO: Add some validation steps to ensure that the IgntionData is compliant, if necessary.
 			// Assume for now, that it's going to json format.
@@ -88,22 +85,12 @@ func (r *IPXEBootConfigReconciler) ensureIgnition(ctx context.Context, _ logr.Lo
 }
 
 func (r *IPXEBootConfigReconciler) delete(_ context.Context, log logr.Logger, _ *bootv1alpha1.IPXEBootConfig) (ctrl.Result, error) {
-	log.V(1).Info("Deleting ipxeBootConfig")
+	log.V(1).Info("Deleting IPXEBootConfig")
 
 	// TODO
 
+	log.V(1).Info("Deleted IPXEBootConfig")
 	return ctrl.Result{}, nil
-}
-
-// SetupWithManager sets up the controller with the Manager.
-func (r *IPXEBootConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&bootv1alpha1.IPXEBootConfig{}).
-		Watches(
-			&corev1.Secret{},
-			handler.EnqueueRequestsFromMapFunc(r.enqueueIPXEBootConfigReferencingIgnitionSecret),
-		).
-		Complete(r)
 }
 
 func (r *IPXEBootConfigReconciler) patchStatus(
@@ -121,29 +108,40 @@ func (r *IPXEBootConfigReconciler) patchStatus(
 }
 
 func (r *IPXEBootConfigReconciler) enqueueIPXEBootConfigReferencingIgnitionSecret(ctx context.Context, secret client.Object) []reconcile.Request {
-	log := log.Log.WithValues("secret", secret.GetName())
+	log := ctrl.LoggerFrom(ctx)
 	secretObj, ok := secret.(*corev1.Secret)
 	if !ok {
 		log.Error(nil, "cant decode object into Secret", secret)
 		return nil
 	}
 
-	list := &bootv1alpha1.IPXEBootConfigList{}
-	if err := r.List(ctx, list, client.InNamespace(secretObj.Namespace)); err != nil {
-		log.Error(err, "failed to list IPXEBootConfig for secret", secret)
+	configList := &bootv1alpha1.IPXEBootConfigList{}
+	if err := r.List(ctx, configList, client.InNamespace(secretObj.Namespace)); err != nil {
+		log.Error(err, "failed to list IPXEBootConfig for Secret", "Secret", client.ObjectKeyFromObject(secretObj))
 		return nil
 	}
 
 	var requests []reconcile.Request
-	for _, ipxeBootConfig := range list.Items {
-		if ipxeBootConfig.Spec.IgnitionSecretRef != nil && ipxeBootConfig.Spec.IgnitionSecretRef.Name == secretObj.Name {
+	for _, config := range configList.Items {
+		if config.Spec.IgnitionSecretRef != nil && config.Spec.IgnitionSecretRef.Name == secretObj.Name {
 			requests = append(requests, reconcile.Request{
 				NamespacedName: types.NamespacedName{
-					Name:      ipxeBootConfig.Name,
-					Namespace: ipxeBootConfig.Namespace,
+					Name:      config.Name,
+					Namespace: config.Namespace,
 				},
 			})
 		}
 	}
 	return requests
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *IPXEBootConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&bootv1alpha1.IPXEBootConfig{}).
+		Watches(
+			&corev1.Secret{},
+			handler.EnqueueRequestsFromMapFunc(r.enqueueIPXEBootConfigReferencingIgnitionSecret),
+		).
+		Complete(r)
 }
